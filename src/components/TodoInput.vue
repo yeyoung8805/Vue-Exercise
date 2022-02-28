40c7d3d87211638b977e4623ba7550317c22f0d2
yeyoung8805 <yyk8805@naver.com>
2022-02-28 17:57:36 +0900

    edit Header and Input class

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,14 +1,59 @@
 <template>
-    <div>
-        TodoInput
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="material-icons addBtn">add</i>
+        </span>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function() {
+            if(this.newTodoItem != ''){
+                var obj = {completed: false, item:this.newTodoItem};
+                console.log(this.newTodoItem);
+                //저장하는 로직
+                localStorage.setItem(this.newTodoItem, JSON.stringify(obj)); // 스트링으로 변경되도록 함
+                this.clearInput();
+            }
+        },
+        clearInput: function () {
+            this.newTodoItem = '';
+        }
+    },
 }
 </script>
 
-<style>
+<style scoped>
+input:focus {
+    outline: none;
+}
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #62EAC6,#32CEE6);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
 </style>
